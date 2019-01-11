0f60243458f97896d4f282a62932e90f8a6a943a
Branden Fung <bfung@pdftron.com>
2019-01-11 11:17:49 -0800

    Initial repository commit

---
+++ CustomStickyNote/app/src/main/java/com/pdftron/android/samples/stickynote/ReaderActivity.java
@@ -0,0 +1,21 @@
+package com.pdftron.android.samples.stickynote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pdftron.pdf.controls.DocumentActivity;
+
+/**
+ * ReaderActivity is just used to start a DocumentActivity so we can try out our new Sticky Note Icon.
+ */
+public class ReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+        // Open our sample document in the 'res/raw' resource folder
+        DocumentActivity.openDocument(this, R.raw.sample);
+        finish();
+    }
+}

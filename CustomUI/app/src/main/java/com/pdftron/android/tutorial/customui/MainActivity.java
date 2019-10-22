9c2935d59e86aeeb3fdd90d8916d6ed8167c8f26
sgong-pdftron <sgong@pdftron.com>
2019-10-22 12:21:42 -0700

    demo custom bottom layout

--- CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
+++ CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
@@ -1,20 +1,27 @@
 package com.pdftron.android.tutorial.customui;
 
+import android.app.AlertDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.pdftron.android.tutorial.customui.custom.CustomAnnotationToolbar;
 import com.pdftron.android.tutorial.customui.custom.CustomLinkClick;
 import com.pdftron.android.tutorial.customui.custom.CustomQuickMenu;
+import com.pdftron.pdf.Annot;
 import com.pdftron.pdf.config.ViewerBuilder;
+import com.pdftron.pdf.config.ViewerConfig;
 import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment;
 import com.pdftron.pdf.model.FileInfo;
+import com.pdftron.pdf.tools.ToolManager;
 import com.pdftron.pdf.utils.Utils;
 
 import java.io.File;
@@ -31,9 +38,18 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
         // Instantiate a PdfViewCtrlTabHostFragment with a document Uri
         File f = Utils.copyResourceToLocal(this, R.raw.sample, "sample", ".pdf");
         Uri uri = Uri.fromFile(f);
+        ViewerConfig config = new ViewerConfig.Builder()
+                .fullscreenModeEnabled(false)
+                .multiTabEnabled(false)
+                .toolbarTitle("Reader")
+                .showUserBookmarksList(false)
+                .showAnnotationsList(false)
+                .showBottomNavBar(false)
+                .build();
         mPdfViewCtrlTabHostFragment = ViewerBuilder.withUri(uri)
-                .usingCustomToolbar(new int[] {R.menu.my_custom_options_toolbar})
+                .usingCustomToolbar(new int[]{R.menu.my_custom_options_toolbar})
                 .usingNavIcon(R.drawable.ic_star_white_24dp)
+                .usingConfig(config)
                 .build(this);
         mPdfViewCtrlTabHostFragment.addHostListener(this);
 
@@ -41,6 +57,37 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, mPdfViewCtrlTabHostFragment);
         ft.commit();
+
+        ImageButton outlineButton = findViewById(R.id.outline);
+        outlineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPdfViewCtrlTabHostFragment != null) {
+                    mPdfViewCtrlTabHostFragment.onOutlineOptionSelected();
+                }
+            }
+        });
+
+        ImageButton drawButton = findViewById(R.id.draw);
+        drawButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPdfViewCtrlTabHostFragment != null) {
+                    mPdfViewCtrlTabHostFragment.onInkEditSelected(null, 0);
+                }
+            }
+        });
+
+        ImageButton signatureButton = findViewById(R.id.signature);
+        signatureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPdfViewCtrlTabHostFragment != null && mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment() != null) {
+                    ToolManager tm = mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getToolManager();
+                    tm.setTool(tm.createTool(ToolManager.ToolMode.SIGNATURE, null));
+                }
+            }
+        });
     }
 
     @Override
@@ -56,6 +103,40 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
         new CustomQuickMenu(MainActivity.this, mPdfViewCtrlTabHostFragment).applyCustomization();
         new CustomLinkClick(MainActivity.this, mPdfViewCtrlTabHostFragment).applyCustomization();
         new CustomAnnotationToolbar(MainActivity.this, mPdfViewCtrlTabHostFragment).applyCustomization();
+
+        if (mPdfViewCtrlTabHostFragment != null && mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment() != null) {
+            ToolManager tm = mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getToolManager();
+            tm.setBasicAnnotationListener(new ToolManager.BasicAnnotationListener() {
+                @Override
+                public void onAnnotationSelected(Annot annot, int i) {
+                    mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().onAnnotationSelected(annot, i);
+                }
+
+                @Override
+                public void onAnnotationUnselected() {
+                    mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().onAnnotationUnselected();
+                }
+
+                @Override
+                public boolean onInterceptAnnotationHandling(@Nullable Annot annot, Bundle bundle, ToolManager.ToolMode toolMode) {
+                    try {
+                        // Intercept clicking widget annotation by return true
+                        if (annot != null && annot.getType() == Annot.e_Widget) {
+                            Toast.makeText(MainActivity.this, "Form widget clicked", Toast.LENGTH_SHORT).show();
+                            return true; // return true to perform custom action
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                }
+
+                @Override
+                public boolean onInterceptDialog(AlertDialog alertDialog) {
+                    return false;
+                }
+            });
+        }
     }
 
     @Override

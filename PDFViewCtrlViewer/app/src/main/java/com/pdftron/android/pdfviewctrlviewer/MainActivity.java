9a4d325ccb4c1a31d64ef3dd0984879bfa5e14d7
Branden Fung <bfung@pdftron.com>
2020-04-09 22:20:41 -0700

    Add button to launch activity

--- PDFViewCtrlViewer/app/src/main/java/com/pdftron/android/pdfviewctrlviewer/MainActivity.java
+++ PDFViewCtrlViewer/app/src/main/java/com/pdftron/android/pdfviewctrlviewer/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pdftron.android.pdfviewctrlviewer;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.pdftron.common.PDFNetException;
 import com.pdftron.pdf.PDFDoc;
@@ -32,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPdfViewCtrl = findViewById(R.id.pdfviewctrl);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
         setupToolManager();
         setupAnnotationToolbar();
         try {

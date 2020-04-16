6b26ab101d35fe25e5bf290148d545daf9dd9e0b
sgong <sgong@pdftron.com>
2020-04-16 15:31:48 -0700

    demo move annotation to the next page

--- CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/custom/CustomLinkClick.java
+++ CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/custom/CustomLinkClick.java
@@ -11,11 +11,15 @@ import com.pdftron.android.tutorial.customui.R;
 import com.pdftron.common.PDFNetException;
 import com.pdftron.pdf.Annot;
 import com.pdftron.pdf.PDFViewCtrl;
+import com.pdftron.pdf.Page;
+import com.pdftron.pdf.annots.Markup;
 import com.pdftron.pdf.controls.PdfViewCtrlTabFragment;
 import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment;
 import com.pdftron.pdf.tools.CustomRelativeLayout;
 import com.pdftron.pdf.tools.Tool;
 import com.pdftron.pdf.tools.ToolManager;
+
+import java.util.Map;
 
 /**
  * Delegate class that adds a custom view on top of a link annotation when the annotation
@@ -41,6 +45,80 @@ public class CustomLinkClick extends CustomizationDelegate {
         final ToolManager toolManager = tabFragment.getToolManager();
 
         if (pdfViewCtrl != null && toolManager != null) {
+            toolManager.addAnnotationModificationListener(new ToolManager.AnnotationModificationListener() {
+                @Override
+                public void onAnnotationsAdded(Map<Annot, Integer> map) {
+
+                }
+
+                @Override
+                public void onAnnotationsPreModify(Map<Annot, Integer> map) {
+
+                }
+
+                @Override
+                public void onAnnotationsModified(Map<Annot, Integer> map, Bundle bundle) {
+                    for (Map.Entry<Annot, Integer> entry : map.entrySet()) {
+                        boolean shouldUnlock = false;
+                        try {
+                            pdfViewCtrl.docLock(true);
+                            shouldUnlock = true;
+
+                            Annot annot = entry.getKey();
+                            int pageNum = entry.getValue();
+
+                            int newPageNum = pageNum + 1;
+
+                            Page oldPage = pdfViewCtrl.getDoc().getPage(pageNum);
+                            Page newPage = pdfViewCtrl.getDoc().getPage(newPageNum);
+
+                            Annot pp = null;
+                            if (annot.isMarkup()) {
+                                pp = new Markup(annot).getPopup();
+                            }
+                            oldPage.annotRemove(annot);
+                            newPage.annotPushBack(annot);
+                            if (pp != null) {
+                                oldPage.annotRemove(pp);
+                                newPage.annotPushBack(pp);
+                            }
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        } finally {
+                            if (shouldUnlock) {
+                                pdfViewCtrl.docUnlock();
+                            }
+                        }
+                    }
+                    toolManager.deselectAll();
+                    try {
+                        pdfViewCtrl.update(true);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onAnnotationsPreRemove(Map<Annot, Integer> map) {
+
+                }
+
+                @Override
+                public void onAnnotationsRemoved(Map<Annot, Integer> map) {
+
+                }
+
+                @Override
+                public void onAnnotationsRemovedOnPage(int i) {
+
+                }
+
+                @Override
+                public void annotationsCouldNotBeAdded(String s) {
+
+                }
+            });
+
             toolManager.setBasicAnnotationListener(new ToolManager.BasicAnnotationListener() {
                 @Override
                 public void onAnnotationSelected(Annot annot, int i) {

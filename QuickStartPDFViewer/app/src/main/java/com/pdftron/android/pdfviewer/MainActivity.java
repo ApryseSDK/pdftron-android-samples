00ba28a2c5a711c1d966ec30782954f103a69c5e
Branden Fung <bfung@pdftron.com>
2020-05-04 12:38:18 -0700

    Add sample

--- QuickStartPDFViewer/app/src/main/java/com/pdftron/android/pdfviewer/MainActivity.java
+++ QuickStartPDFViewer/app/src/main/java/com/pdftron/android/pdfviewer/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Open our sample document in the 'res/raw' resource folder
         openRawResourceDocument(this, R.raw.sample);
-
-        finish();
     }
 
     /**

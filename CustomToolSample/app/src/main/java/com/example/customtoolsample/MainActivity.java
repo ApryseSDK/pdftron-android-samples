aba97da7a1dabc9d27130b008a0970d380bcec53
sgong <sgong@pdftron.com>
2020-08-12 10:11:54 -0700

    Update to new ui

--- CustomToolSample/app/src/main/java/com/example/customtoolsample/MainActivity.java
+++ CustomToolSample/app/src/main/java/com/example/customtoolsample/MainActivity.java
@@ -10,9 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import com.pdftron.pdf.config.ToolManagerBuilder;
-import com.pdftron.pdf.config.ViewerBuilder;
+import com.pdftron.pdf.config.ViewerBuilder2;
 import com.pdftron.pdf.config.ViewerConfig;
-import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment;
+import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment2;
 import com.pdftron.pdf.tools.ToolManager;
 import com.pdftron.pdf.utils.Utils;
 
@@ -20,7 +20,7 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    PdfViewCtrlTabHostFragment fr = null;
+    PdfViewCtrlTabHostFragment2 fr = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Add a viewer fragment to the layout container in the specified
     // activity, and returns the added fragment
-    public PdfViewCtrlTabHostFragment addViewerFragment(@IdRes int fragmentContainer,
+    public PdfViewCtrlTabHostFragment2 addViewerFragment(@IdRes int fragmentContainer,
             @NonNull FragmentActivity activity, @NonNull Uri fileUri, @Nullable String password) {
 
         ToolManagerBuilder toolManagerBuilder = ToolManagerBuilder
@@ -47,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         // Create the viewer fragment
-        PdfViewCtrlTabHostFragment fragment =
-                ViewerBuilder.withUri(fileUri, password)
+        PdfViewCtrlTabHostFragment2 fragment =
+                ViewerBuilder2.withUri(fileUri, password)
                         .usingConfig(config)
                         .build(activity);
 
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         useCustomStamp(fr);
     }
 
-    public void useCloudSquare(@NonNull PdfViewCtrlTabHostFragment fragment) {
+    public void useCloudSquare(@NonNull PdfViewCtrlTabHostFragment2 fragment) {
         // Create our custom tool
         ToolManager toolManager = fragment.getCurrentPdfViewCtrlFragment().getToolManager();
         ToolManager.Tool customTool = toolManager.createTool(CustomCloudSquare.MODE, toolManager.getTool());
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         toolManager.setTool(customTool);
     }
 
-    public void useCustomStamp(@NonNull PdfViewCtrlTabHostFragment fragment) {
+    public void useCustomStamp(@NonNull PdfViewCtrlTabHostFragment2 fragment) {
         // Create our custom tool
         ToolManager toolManager = fragment.getCurrentPdfViewCtrlFragment().getToolManager();
         ToolManager.Tool customTool = toolManager.createTool(CustomStamp.MODE, toolManager.getTool());

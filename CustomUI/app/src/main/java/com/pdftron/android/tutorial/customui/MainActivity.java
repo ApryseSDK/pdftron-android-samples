20f8b83363cb18d6da5483f47b84aec186c72917
sgong <sgong@pdftron.com>
2020-10-19 14:49:14 -0700

    demo custom color picker

--- CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
+++ CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
@@ -7,22 +7,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.pdftron.android.tutorial.customui.custom.CustomAnnotationToolbar;
 import com.pdftron.android.tutorial.customui.custom.CustomLinkClick;
 import com.pdftron.android.tutorial.customui.custom.CustomQuickMenu;
+import com.pdftron.pdf.Annot;
 import com.pdftron.pdf.config.ViewerBuilder;
 import com.pdftron.pdf.config.ViewerConfig;
 import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment;
 import com.pdftron.pdf.model.FileInfo;
+import com.pdftron.pdf.tools.AnnotEdit;
+import com.pdftron.pdf.tools.AnnotEditTextMarkup;
+import com.pdftron.pdf.tools.Tool;
+import com.pdftron.pdf.tools.ToolManager;
 import com.pdftron.pdf.utils.Utils;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHostFragment.TabHostListener {
 
     private PdfViewCtrlTabHostFragment mPdfViewCtrlTabHostFragment;
+
+    private HashMap<Annot, Integer> mSelectedAnnots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +45,82 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
         Uri uri = Uri.fromFile(f);
         ViewerConfig viewerConfig = new ViewerConfig.Builder()
                 .toolbarTitle("٩(◕‿◕｡)۶")
+                .showBottomNavBar(false)
+                .fullscreenModeEnabled(false)
                 .build();
         mPdfViewCtrlTabHostFragment = ViewerBuilder.withUri(uri)
-                .usingCustomToolbar(new int[] {R.menu.my_custom_options_toolbar})
+                .usingCustomToolbar(new int[]{R.menu.my_custom_options_toolbar})
                 .usingNavIcon(R.drawable.ic_star_white_24dp)
                 .usingConfig(viewerConfig)
                 .build(this);
         mPdfViewCtrlTabHostFragment.addHostListener(this);
 
         // Apply customizations to tab host fragment
-        new CustomQuickMenu(MainActivity.this, mPdfViewCtrlTabHostFragment);
-        new CustomLinkClick(MainActivity.this, mPdfViewCtrlTabHostFragment);
-        new CustomAnnotationToolbar(MainActivity.this, mPdfViewCtrlTabHostFragment);
+//        new CustomQuickMenu(MainActivity.this, mPdfViewCtrlTabHostFragment);
+//        new CustomLinkClick(MainActivity.this, mPdfViewCtrlTabHostFragment);
+//        new CustomAnnotationToolbar(MainActivity.this, mPdfViewCtrlTabHostFragment);
+
+        // custom style editing
+
+        View redBtn = findViewById(R.id.btn_red);
+        View blueBtn = findViewById(R.id.btn_blue);
+        View yellowBtn = findViewById(R.id.btn_yellow);
+
+        redBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editColor(getResources().getColor(R.color.red));
+            }
+        });
+        blueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editColor(getResources().getColor(R.color.blue));
+            }
+        });
+        yellowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editColor(getResources().getColor(R.color.yellow));
+            }
+        });
 
         // Add the fragment to our activity
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, mPdfViewCtrlTabHostFragment);
         ft.commit();
+    }
+
+    private void editColor(int color) {
+        if (mPdfViewCtrlTabHostFragment == null) {
+            return;
+        }
+        try {
+            ToolManager toolManager = mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getToolManager();
+            Tool currentTool = (Tool) toolManager.getTool();
+            if (mSelectedAnnots != null) {
+                for (Map.Entry<Annot, Integer> entry : mSelectedAnnots.entrySet()) {
+                    Annot annot = entry.getKey();
+                    Integer page = entry.getValue();
+                    if (currentTool instanceof AnnotEdit) {
+                        AnnotEdit tool = (AnnotEdit) currentTool;
+                         if (annot.getType() == Annot.e_Ink) {
+                            tool.onChangeAnnotStrokeColor(color);
+                        } else if (annot.getType() == Annot.e_FreeText) {
+                            tool.onChangeAnnotTextColor(color);
+                        }
+                    } else if (currentTool instanceof AnnotEditTextMarkup) {
+                        AnnotEdit annotEdit = (AnnotEdit) toolManager.createTool(ToolManager.ToolMode.ANNOT_EDIT, toolManager.getTool());
+                        if (annot.getType() == Annot.e_Highlight) {
+                            annotEdit.onChangeAnnotStrokeColor(color);
+                        }
+                        toolManager.selectAnnot(annot, page);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -63,6 +133,14 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
 
     @Override
     public void onTabDocumentLoaded(String s) {
+        if (mPdfViewCtrlTabHostFragment != null) {
+            mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getToolManager().addAnnotationsSelectionListener(new ToolManager.AnnotationsSelectionListener() {
+                @Override
+                public void onAnnotationsSelectionChanged(HashMap<Annot, Integer> hashMap) {
+                    mSelectedAnnots = hashMap;
+                }
+            });
+        }
     }
 
     @Override

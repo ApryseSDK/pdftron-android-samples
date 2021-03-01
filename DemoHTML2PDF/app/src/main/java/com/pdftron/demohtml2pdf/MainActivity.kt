c0a9846708d3323adf1b69055d1b190cdb425070
sgong <sgong@pdftron.com>
2021-03-01 12:35:24 -0800

    test

--- DemoHTML2PDF/app/src/main/java/com/pdftron/demohtml2pdf/MainActivity.kt
+++ DemoHTML2PDF/app/src/main/java/com/pdftron/demohtml2pdf/MainActivity.kt
@@ -16,33 +16,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val htmlDocument = "<!DOCTYPE html>\n" +
-                "<html lang=\"de\">\n" +
-                "\n" +
+                "<html>\n" +
+                "<!-- Text between angle brackets is an HTML tag and is not displayed.\n" +
+                "Most tags, such as the HTML and /HTML tags that surround the contents of\n" +
+                "a page, come in pairs; some tags, like HR, for a horizontal rule, stand \n" +
+                "alone. Comments, such as the text you're reading, are not displayed when\n" +
+                "the Web page is shown. The information between the HEAD and /HEAD tags is \n" +
+                "not displayed. The information between the BODY and /BODY tags is displayed.-->\n" +
                 "<head>\n" +
-                " <meta charset=\"UTF-8\" />\n" +
-                " <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
-                " <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />\n" +
-                " <title>Test Page</title>\n" +
-                " <style>\n" +
-                " body {\n" +
-                " font-family: Helvetica, Arial, sans-serif;\n" +
-                " padding: 10px;\n" +
-                " -webkit-hyphens: auto;\n" +
-                " -moz-hyphens: auto;\n" +
-                " hyphens: auto;\n" +
-                " }\n" +
-                "\n" +
-                " h2 {\n" +
-                " width: 100%;\n" +
-                " border-bottom: 1px solid #000000;\n" +
-                " }\n" +
-                "\n" +
-                " </style>\n" +
+                "<title>Enter a title, displayed at the top of the window.</title>\n" +
                 "</head>\n" +
-                "\n" +
+                "<!-- The information between the BODY and /BODY tags is displayed.-->\n" +
                 "<body>\n" +
-                "<h2>Hello World</h2>\n" +
-                "<p> Test Text </p>\n" +
+                "<h1>Enter the main heading, usually the same as the title.</h1>\n" +
+                "<p>Be <b>bold</b> in stating your key points. Put them in a list: </p>\n" +
+                "<ul>\n" +
+                "<li>The first item in your list</li>\n" +
+                "<li>The second item; <i>italicize</i> key words</li>\n" +
+                "</ul>\n" +
+                "<p>Improve your image by including an image. </p>\n" +
+                "<p><img src=\"http://www.mygifs.com/CoverImage.gif\" alt=\"A Great HTML Resource\"></p>\n" +
+                "<p>Add a link to your favorite <a href=\"https://www.dummies.com/\">Web site</a>.\n" +
+                "Break up your page with a horizontal rule or two. </p>\n" +
+                "<hr>\n" +
+                "<p>Finally, link to <a href=\"page2.html\">another page</a> in your own Web site.</p>\n" +
+                "<!-- And add a copyright notice.-->\n" +
+                "<p>&#169; Wiley Publishing, 2011</p>\n" +
                 "</body>\n" +
                 "</html>"
 

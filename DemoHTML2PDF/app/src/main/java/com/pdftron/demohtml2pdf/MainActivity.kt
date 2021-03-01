7daaa6f357b09ca37ffe521b5cb4020ca5749d4b
sgong <sgong@pdftron.com>
2021-03-01 12:44:35 -0800

    use local file instead

--- DemoHTML2PDF/app/src/main/java/com/pdftron/demohtml2pdf/MainActivity.kt
+++ DemoHTML2PDF/app/src/main/java/com/pdftron/demohtml2pdf/MainActivity.kt
@@ -3,10 +3,12 @@ package com.pdftron.demohtml2pdf
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.pdftron.pdf.config.ViewerConfig
 import com.pdftron.pdf.controls.DocumentActivity
 import com.pdftron.pdf.utils.HTML2PDF
+import com.pdftron.pdf.utils.Utils
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,37 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val htmlDocument = "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<!-- Text between angle brackets is an HTML tag and is not displayed.\n" +
-                "Most tags, such as the HTML and /HTML tags that surround the contents of\n" +
-                "a page, come in pairs; some tags, like HR, for a horizontal rule, stand \n" +
-                "alone. Comments, such as the text you're reading, are not displayed when\n" +
-                "the Web page is shown. The information between the HEAD and /HEAD tags is \n" +
-                "not displayed. The information between the BODY and /BODY tags is displayed.-->\n" +
-                "<head>\n" +
-                "<title>Enter a title, displayed at the top of the window.</title>\n" +
-                "</head>\n" +
-                "<!-- The information between the BODY and /BODY tags is displayed.-->\n" +
-                "<body>\n" +
-                "<h1>Enter the main heading, usually the same as the title.</h1>\n" +
-                "<p>Be <b>bold</b> in stating your key points. Put them in a list: </p>\n" +
-                "<ul>\n" +
-                "<li>The first item in your list</li>\n" +
-                "<li>The second item; <i>italicize</i> key words</li>\n" +
-                "</ul>\n" +
-                "<p>Improve your image by including an image. </p>\n" +
-                "<p><img src=\"http://www.mygifs.com/CoverImage.gif\" alt=\"A Great HTML Resource\"></p>\n" +
-                "<p>Add a link to your favorite <a href=\"https://www.dummies.com/\">Web site</a>.\n" +
-                "Break up your page with a horizontal rule or two. </p>\n" +
-                "<hr>\n" +
-                "<p>Finally, link to <a href=\"page2.html\">another page</a> in your own Web site.</p>\n" +
-                "<!-- And add a copyright notice.-->\n" +
-                "<p>&#169; Wiley Publishing, 2011</p>\n" +
-                "</body>\n" +
-                "</html>"
+        val htmlFile =
+            Utils.copyResourceToLocal(this, R.raw.test, "test", ".html")
 
-        val html2PDF = HTML2PDF(this)
+        val webView = WebView(this)
+        webView.loadUrl("file://" + htmlFile.absolutePath)
+
+        val html2PDF = HTML2PDF(webView)
         html2PDF.setOutputFolder(this.cacheDir)
         html2PDF.setHTML2PDFListener(object : HTML2PDF.HTML2PDFListener {
             override fun onConversionFinished(pdfOutput: String, isLocal: Boolean) {
@@ -60,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                 Log.e("html2pdf", error)
             }
         })
-        html2PDF.fromHTMLDocument(null, htmlDocument)
+        html2PDF.doHtml2Pdf()
     }
 
     fun openDocument(filepath: String) {

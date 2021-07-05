83903bd3d7a7e31372bc2a7c45a43e14a3a8ae50
sgong <sgong@pdftron.com>
2021-07-05 12:14:03 -0700

    demo view pager

--- PDFViewCtrlViewer/app/src/main/java/com/pdftron/android/pdfviewctrlviewer/MainActivity.java
+++ PDFViewCtrlViewer/app/src/main/java/com/pdftron/android/pdfviewctrlviewer/MainActivity.java
@@ -1,104 +1,35 @@
 package com.pdftron.android.pdfviewctrlviewer;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.FrameLayout;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.pdftron.common.PDFNetException;
 import com.pdftron.pdf.PDFDoc;
 import com.pdftron.pdf.PDFViewCtrl;
-import com.pdftron.pdf.config.ToolManagerBuilder;
-import com.pdftron.pdf.tools.ToolManager;
-import com.pdftron.pdf.utils.AppUtils;
 import com.pdftron.pdf.utils.Utils;
-import com.pdftron.pdf.widget.preset.component.PresetBarComponent;
-import com.pdftron.pdf.widget.preset.component.PresetBarViewModel;
-import com.pdftron.pdf.widget.preset.component.view.PresetBarView;
-import com.pdftron.pdf.widget.toolbar.ToolManagerViewModel;
-import com.pdftron.pdf.widget.toolbar.builder.AnnotationToolbarBuilder;
-import com.pdftron.pdf.widget.toolbar.builder.ToolbarButtonType;
-import com.pdftron.pdf.widget.toolbar.component.AnnotationToolbarComponent;
-import com.pdftron.pdf.widget.toolbar.component.AnnotationToolbarViewModel;
-import com.pdftron.pdf.widget.toolbar.component.DefaultToolbars;
-import com.pdftron.pdf.widget.toolbar.component.view.AnnotationToolbarView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
 
-    private PDFViewCtrl mPdfViewCtrl;
-    private PDFDoc mPdfDoc;
-    private ToolManager mToolManager;
-    private AnnotationToolbarComponent mAnnotationToolbarComponent;
-    private PresetBarComponent mPresetBarComponent;
-    private FrameLayout mToolbarContainer;
-    private FrameLayout mPresetContainer;
+    private final SparseArray<PDFViewCtrl> mViewHolders = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPdfViewCtrl = findViewById(R.id.pdfviewctrl);
-        mToolbarContainer = findViewById(R.id.annotatio_toolbar_container);
-        mPresetContainer = findViewById(R.id.preset_bar);
-        setupToolManager();
-        setupAnnotationToolbar();
-        try {
-            AppUtils.setupPDFViewCtrl(mPdfViewCtrl);
-            viewFromResource(R.raw.sample, "sample_file");
-        } catch (PDFNetException e) {
-            Log.e(TAG, "Error setting up PDFViewCtrl");
-        }
-    }
-
-    /**
-     * Helper method to set up and initialize the ToolManager.
-     */
-    public void setupToolManager() {
-        mToolManager = ToolManagerBuilder.from()
-                .build(this, mPdfViewCtrl);
-    }
-
-    /**
-     * Helper method to set up and initialize the AnnotationToolbar.
-     */
-    public void setupAnnotationToolbar() {
-        ToolManagerViewModel toolManagerViewModel = ViewModelProviders.of(this).get(ToolManagerViewModel.class);
-        toolManagerViewModel.setToolManager(mToolManager);
-        PresetBarViewModel presetViewModel = ViewModelProviders.of(this).get(PresetBarViewModel.class);
-        AnnotationToolbarViewModel annotationToolbarViewModel = ViewModelProviders.of(this).get(AnnotationToolbarViewModel.class);
-
-        // Create our UI components for the annotation toolbar annd preset bar
-        mAnnotationToolbarComponent = new AnnotationToolbarComponent(
-                this,
-                annotationToolbarViewModel,
-                presetViewModel,
-                toolManagerViewModel,
-                new AnnotationToolbarView(mToolbarContainer)
-        );
-
-        mPresetBarComponent = new PresetBarComponent(
-                this,
-                getSupportFragmentManager(),
-                presetViewModel,
-                toolManagerViewModel,
-                new PresetBarView(mPresetContainer)
-        );
-
-        // Create our custom toolbar and pass it to the annotation toolbar UI component
-        mAnnotationToolbarComponent.inflateWithBuilder(
-                AnnotationToolbarBuilder.withTag("Custom Toolbar")
-                        .addToolButton(ToolbarButtonType.SQUARE, DefaultToolbars.ButtonId.SQUARE.value())
-                        .addToolButton(ToolbarButtonType.INK, DefaultToolbars.ButtonId.INK.value())
-                        .addToolButton(ToolbarButtonType.FREE_HIGHLIGHT, DefaultToolbars.ButtonId.FREE_HIGHLIGHT.value())
-                        .addToolButton(ToolbarButtonType.ERASER, DefaultToolbars.ButtonId.ERASER.value())
-                        .addToolStickyButton(ToolbarButtonType.UNDO, DefaultToolbars.ButtonId.UNDO.value())
-                        .addToolStickyButton(ToolbarButtonType.REDO, DefaultToolbars.ButtonId.REDO.value())
-        );
+        // setup pager
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(new CustomPagerAdapter());
     }
 
     /**
@@ -108,52 +39,60 @@ public class MainActivity extends AppCompatActivity {
      * @param fileName   of the temporary PDF file copy
      * @throws PDFNetException if invalid document path is supplied to PDFDoc
      */
-    public void viewFromResource(int resourceId, String fileName) throws PDFNetException {
+    public void viewFromResource(PDFViewCtrl pdfViewCtrl, int resourceId, String fileName) throws PDFNetException {
         File file = Utils.copyResourceToLocal(this, resourceId, fileName, ".pdf");
-        mPdfDoc = new PDFDoc(file.getAbsolutePath());
-        mPdfViewCtrl.setDoc(mPdfDoc);
+        PDFDoc pdfDoc = new PDFDoc(file.getAbsolutePath());
+        pdfViewCtrl.setDoc(pdfDoc);
         // Alternatively, you can open the document using Uri:
         // Uri fileUri = Uri.fromFile(file);
-        // mPdfDoc = mPdfViewCtrl.openPDFUri(fileUri, null);
+        // PDFDoc pdfDoc = pdfViewCtrl.openPDFUri(fileUri, null);
     }
 
-    /**
-     * We need to clean up and handle PDFViewCtrl based on Android lifecycle callback.
-     */
+    class CustomPagerAdapter extends PagerAdapter {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mPdfViewCtrl != null) {
-            mPdfViewCtrl.pause();
-            mPdfViewCtrl.purgeMemory();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mPdfViewCtrl != null) {
-            mPdfViewCtrl.resume();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mPdfViewCtrl != null) {
-            mPdfViewCtrl.destroy();
-            mPdfViewCtrl = null;
+        @Override
+        public int getCount() {
+            return 3;
         }
 
-        if (mPdfDoc != null) {
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            LayoutInflater inflater = LayoutInflater.from(container.getContext());
+            View layout = inflater.inflate(R.layout.viewer_page, container, false);
+            PDFViewCtrl pdfViewCtrl = layout.findViewById(R.id.pdfviewctrl);
+
+            // load doc
             try {
-                mPdfDoc.close();
-            } catch (Exception e) {
-                // handle exception
-            } finally {
-                mPdfDoc = null;
+                if (position == 0) {
+                    viewFromResource(pdfViewCtrl, R.raw.floorplan, "floor_plan");
+                } else if (position == 1) {
+                    viewFromResource(pdfViewCtrl, R.raw.sample, "sample");
+                } else {
+                    viewFromResource(pdfViewCtrl, R.raw.words, "words");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+
+            mViewHolders.put(position, pdfViewCtrl);
+            container.addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            PDFViewCtrl pdfViewCtrl = mViewHolders.get(position);
+            if (pdfViewCtrl != null) {
+                pdfViewCtrl.destroy();
+                pdfViewCtrl = null;
+            }
+            container.removeView((View) object);
         }
     }
 }

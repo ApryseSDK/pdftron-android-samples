2d58c4582f3cb8f43290d610d8950775d5b258f0
sgong <sgong@pdftron.com>
2022-03-28 08:09:56 -0700

    Demo annotation data and XFDF data

--- CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
+++ CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
@@ -2,29 +2,38 @@ package com.pdftron.android.tutorial.customui;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.pdftron.android.tutorial.customui.custom.CustomAnnotationToolbar;
 import com.pdftron.android.tutorial.customui.custom.CustomLinkClick;
 import com.pdftron.android.tutorial.customui.custom.CustomQuickMenu;
+import com.pdftron.fdf.FDFDoc;
+import com.pdftron.pdf.Annot;
+import com.pdftron.pdf.PDFDoc;
+import com.pdftron.pdf.annots.Markup;
 import com.pdftron.pdf.config.ViewerBuilder2;
 import com.pdftron.pdf.config.ViewerConfig;
-import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment;
 import com.pdftron.pdf.controls.PdfViewCtrlTabHostFragment2;
 import com.pdftron.pdf.model.FileInfo;
+import com.pdftron.pdf.tools.ToolManager;
 import com.pdftron.pdf.utils.Utils;
 import com.pdftron.pdf.widget.toolbar.builder.AnnotationToolbarBuilder;
 import com.pdftron.pdf.widget.toolbar.builder.ToolbarButtonType;
 import com.pdftron.pdf.widget.toolbar.component.DefaultToolbars;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHostFragment.TabHostListener {
+public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHostFragment2.TabHostListener {
 
     private PdfViewCtrlTabHostFragment2 mPdfViewCtrlTabHostFragment;
 
@@ -45,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
                 .toolbarTitle("٩(◕‿◕｡)۶")
                 .build();
         mPdfViewCtrlTabHostFragment = ViewerBuilder2.withUri(uri)
-                .usingCustomToolbar(new int[] {R.menu.my_custom_options_toolbar})
+                .usingCustomToolbar(new int[]{R.menu.my_custom_options_toolbar})
                 .usingNavIcon(R.drawable.ic_star_white_24dp)
                 .usingConfig(viewerConfig)
                 .usingTheme(R.style.CustomAppTheme)
@@ -83,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
                 .addToolStickyButton(ToolbarButtonType.REDO, DefaultToolbars.ButtonId.REDO.value());
     }
 
-
     private AnnotationToolbarBuilder buildShapesToolbar() {
         return AnnotationToolbarBuilder.withTag(SHAPES_TOOLBAR_TAG) // Identifier for toolbar
                 .setToolbarName("Shapes Toolbar") // Name used when displaying toolbar
@@ -98,6 +106,45 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
 
     @Override
     public void onTabDocumentLoaded(String s) {
+        if (mPdfViewCtrlTabHostFragment != null && mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment() != null) {
+            ToolManager tm = mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getToolManager();
+            tm.addAnnotationModificationListener(new ToolManager.AnnotationModificationListener() {
+                @Override
+                public void onAnnotationsAdded(Map<Annot, Integer> annots) {
+                    demoExtraAnnotData("onAnnotationsAdded", annots);
+                }
+
+                @Override
+                public void onAnnotationsPreModify(Map<Annot, Integer> annots) {
+
+                }
+
+                @Override
+                public void onAnnotationsModified(Map<Annot, Integer> annots, Bundle extra) {
+                    demoExtraAnnotData("onAnnotationsModified", annots);
+                }
+
+                @Override
+                public void onAnnotationsPreRemove(Map<Annot, Integer> annots) {
+                    demoExtraAnnotData("onAnnotationsPreRemove", annots);
+                }
+
+                @Override
+                public void onAnnotationsRemoved(Map<Annot, Integer> annots) {
+
+                }
+
+                @Override
+                public void onAnnotationsRemovedOnPage(int pageNum) {
+
+                }
+
+                @Override
+                public void annotationsCouldNotBeAdded(String errorMessage) {
+
+                }
+            });
+        }
     }
 
     @Override
@@ -186,5 +233,35 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
     @Override
     public void onJumpToSdCardFolder() {
 
+    }
+
+    private void demoExtraAnnotData(String event, Map<Annot, Integer> annots) {
+        try {
+            for (Annot a : annots.keySet()) {
+                if (a.isMarkup()) {
+                    Markup mu = new Markup(a);
+                    mu.setSubject("New Subject");
+                }
+                a.setCustomData("contactId", UUID.randomUUID().toString());
+            }
+            String xfdf = getXfdf(annots);
+            Log.d("PDFTron", event + ": " + xfdf);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Nullable
+    private String getXfdf(Map<Annot, Integer> annots) {
+        if (mPdfViewCtrlTabHostFragment != null && mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment() != null) {
+            try {
+                PDFDoc pdfDoc = mPdfViewCtrlTabHostFragment.getCurrentPdfViewCtrlFragment().getPdfDoc();
+                FDFDoc fdfDoc = pdfDoc.fdfExtract(new ArrayList<>(annots.keySet()));
+                return fdfDoc.saveAsXFDF();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
     }
 }

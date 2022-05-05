450ba4b3c94ae347f493af554e3f43d1b620f506
sgong <sgong@pdftron.com>
2022-05-05 10:09:08 -0700

    update themes

--- CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
+++ CustomUI/app/src/main/java/com/pdftron/android/tutorial/customui/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements PdfViewCtrlTabHos
                 .usingCustomToolbar(new int[]{R.menu.my_custom_options_toolbar})
                 .usingNavIcon(R.drawable.ic_star_white_24dp)
                 .usingConfig(viewerConfig)
-                .usingTheme(R.style.CustomAppTheme)
+                .usingTheme(R.style.MyCustomAppTheme)
                 .build(this);
         mPdfViewCtrlTabHostFragment.addHostListener(this);
 
